875e07534326fef9f0f8bd1e9cbd21c03c432441
DjMarte <alphat098@gmail.com>
2024-02-12 18:20:12 -0400

    Cree la carpeta Models y agregué el contexto

---
+++ Models/Metas.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial1_AP1_CristopherMarte.Models;
+
+public class Metas
+{
+    [Key]
+    public int MetaId { get; set; }
+
+    public DateTime Fecha { get; set; } = DateTime.Today;
+
+    [RegularExpression(@"^[A-Za-z ]+$", ErrorMessage = "Solo se permiten letras")]
+    [Required(ErrorMessage = "Descripción obligatorio")]
+    public string? Descripcion { get; set; }
+
+    public decimal Monto { get; set; }
+
+}
